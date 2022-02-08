346fdc061f5b2ba7d7a7c4bb688a9f4e5b12879c
Aleksey Petsevich <shpikls@shpikls.ru>
2022-02-08 17:10:01 +0300

    feat: end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let i = 0;
+
+  while (i < str.length) {
+    for (let config of bracketsConfig) {
+      if (
+        str[i] == config[0] 
+        && config[0] == config[1] 
+        && stack[stack.length - 1] == config[0]
+      ) {
+        stack.pop();
+        break;
+      }
+      
+      if (str[i] == config[0]) {
+        stack.push(str[i]);
+      }
+
+      if (str[i] == config[1] && config[0] != config[1]) {
+        if (stack.length == 0) {
+          return false;
+        }
+        
+        if (stack.length != 0 && stack[stack.length - 1] == config[0]) {
+          stack.pop();
+        }
+      }
+        
+    }
+
+    i++;
+
+  }
+  if (stack.length) {
+    return false;
+  }
+  return true;
 }
